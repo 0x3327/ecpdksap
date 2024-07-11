6558ff1a1a3f54a41f0bb2e9a09f90e0705631fe
bojinovic <bojinovicc@hotmail.com>
2024-07-11 10:58:25 +0200

    go: bn254 - added benchmark

---
+++ go/bn254/benchmark/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"ecpdksap-bn254/utils"
+	"encoding/hex"
+	"fmt"
+	"time"
+
+	"github.com/consensys/gnark-crypto/ecc/bn254"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+)
+
+func main () {
+
+	const N = 5000
+
+	fmt.Println("\nBenchmark running with N =", N)
+	var duration time.Duration
+
+	fmt.Println()
+	duration = V0(N);
+	fmt.Println("V0 :: duration:", duration)
+	duration = V0_withViewTag(N)
+	fmt.Println("V0 with view tag:: duration:", duration)
+
+
+	fmt.Println("")
+}
+
+func V0(N int) (duration time.Duration){
+
+	K, v, Rs, _ := generalSetup(N)
+
+	startTime := time.Now()
+
+	for _, Rj := range Rs {
+		utils.RecipientComputesStealthPubKey(&K, &Rj, &v)
+	}
+
+	duration = time.Since(startTime)
+
+	return duration
+}
+
+func V0_withViewTag(N int) (duration time.Duration){
+
+	K, v, Rs, vTags := generalSetup(N)
+
+	startTime := time.Now()
+
+	for idx, Rj := range Rs {
+
+		currVTag := utils.CalculateViewTag(&v, &Rj)
+
+		if vTags[idx] == currVTag {
+			utils.RecipientComputesStealthPubKey(&K, &Rj, &v)
+		}
+	}
+
+	duration = time.Since(startTime)
+
+	return duration
+}
+
+func generalSetup (N int) (K bn254.G2Affine, v fr.Element, Rs []bn254.G1Affine, vTags []uint8) {
+
+	_, K, _ = utils.GenG2KeyPair()
+	v, _, _ = utils.GenG1KeyPair()
+
+	RsString, vTags := utils.GenRandomRs(N)
+
+	var tempR bn254.G1Affine
+	for i := 0 ; i < N; i++ {
+		bytes, _ := hex.DecodeString(RsString[i])
+		tempR.Unmarshal(bytes)
+		Rs = append(Rs, tempR)
+	}
+
+	return K, v, Rs, vTags
+}
