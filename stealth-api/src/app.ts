b2bf2f129356f54cbab0126450a992368ce1c6bd
Aleksandar Veljkovic <aleksandar.veljkovic.dev@gmail.com>
2024-08-21 13:48:26 +0200

    go service

--- stealth-api/src/app.ts
+++ stealth-api/src/app.ts
@@ -1,5 +1,12 @@
 import { API } from '../services/api';
 import BlockchainListener from '../services/blockchain-listener';
+import GoHandler from '../services/go-service';
+
+require('../public/wasm_exec.js');
+
+import path from 'path';
+
+const { readFileSync } = require('fs');
 
 interface AppConfig {
     apiConfig: {
@@ -13,6 +20,7 @@ class App {
     public config: AppConfig;
     public api!: API;
     public blockchainListener!: BlockchainListener;
+    public goHandler!: GoHandler;
 
     constructor(config: AppConfig) {
         this.config = config;
@@ -21,17 +29,22 @@ class App {
     async start(): Promise<void> {
         // Load services
         // this.stealthService = new StealthService(this);
+        this.goHandler = new GoHandler();
         this.api = new API(this);
-        this.blockchainListener = new BlockchainListener(this);
+        // this.blockchainListener = new BlockchainListener(this);
+
+        const metaInfo = await this.goHandler.genSenderInfo();
+        console.log(metaInfo);
         
+
         // Start API
         await this.api.start();
 
-        const currentBLockNumber = await this.blockchainListener.getCurrentBlockNumber();
-        const transactionDetails = await this.blockchainListener.getTransaction('0x16191fcc73ba807341cce0a93b32a63f66b5d28e1580d3ea402331951ced5bec');
+        // const currentBLockNumber = await this.blockchainListener.getCurrentBlockNumber();
+        // const transactionDetails = await this.blockchainListener.getTransaction('0x16191fcc73ba807341cce0a93b32a63f66b5d28e1580d3ea402331951ced5bec');
 
-        console.log('currentBlockNumber: ', currentBLockNumber);
-        console.log('transactionDetails: ', transactionDetails);
+        // console.log('currentBlockNumber: ', currentBLockNumber);
+        // console.log('transactionDetails: ', transactionDetails);
     }
 }
 
