35a2c4e111a886f06826c210a605af061e110ef6
bojinovic <bojinovicc@hotmail.com>
2024-08-27 09:40:36 +0200

    update: build script

--- impl/cli/build.sh
+++ impl/cli/build.sh
@@ -1,5 +1,5 @@
 set -e
 
-# GOAMD64=v4 go build -o builds/ecpdksap-ll-latest
-
-GOARCH=arm64 go build -o builds/ecpdksap-ll-latest-arm64
+# go build -ldflags "-s -w" -o builds/ecpdksap-ll-latest
+GOARCH=arm64 go build -ldflags "-s -w" -o builds/ecpdksap-ll-latest-arm64
+# GOARCH=amd64 go build -ldflags "-s -w" -o builds/ecpdksap-ll-latest-amd64
